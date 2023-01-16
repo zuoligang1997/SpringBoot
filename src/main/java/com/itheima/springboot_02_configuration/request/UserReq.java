7bf664412c317bf799df42b8d0053c1eb6c785bc
zuolg5 <zuolg5@chinaunicom.cn>
2023-01-16 20:28:28 +0800

    框架初始化

---
+++ src/main/java/com/itheima/springboot_02_configuration/request/UserReq.java
@@ -0,0 +1,15 @@
+package com.itheima.springboot_02_configuration.request;
+
+import lombok.Data;
+
+/**
+ * @ClassName UserReq
+ * @Description TODO
+ * @Author Ligang Zuo
+ * @Date 2023-1-16 20:27
+ */
+@Data
+public class UserReq {
+    private String name;
+    private Integer age;
+}
