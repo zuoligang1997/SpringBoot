c4808cff2598ad1a432ebc904b686a54c31a2ba1
zuolg5 <zuolg5@chinaunicom.cn>
2023-01-16 20:14:01 +0800

    框架初始化

---
+++ src/main/java/com/itheima/springboot_02_configuration/Springboot02ConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_02_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02ConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02ConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itheima/springboot_02_configuration/Springboot02ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_02_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02ConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
